adef4fec276ca5d8e9f706e6b6f50fafd489979e
mkfsn <p408865@gmail.com>
2016-09-08 13:44:05 +0900

    Complete 303. Range Sum Query - Immutable

--- algorithm/range-sum-query-immutable/range-sum-query-immutable.cpp
+++ algorithm/range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,22 +1,15 @@
 class NumArray {
-    vector<vector<int>> table;
+    vector<int> table;
 public:
     NumArray(vector<int> &nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            vector<int> tmp;
-            for (int j = 0; j < i; j++) {
-                tmp.push_back(0);
-            }
-            tmp.push_back(nums[i]);
-            for (int j = i + 1; j < nums.size(); j++) {
-                tmp.push_back(tmp[j - 1] + nums[j]);
-            }
-            table.push_back(tmp);
+        table.push_back(0);
+        for (auto n: nums) {
+            table.push_back(table.back() + n);
         }
     }
 
     int sumRange(int i, int j) {
-        return table[i][j];
+        return table[j + 1] - table[i];
     }
 };
 
