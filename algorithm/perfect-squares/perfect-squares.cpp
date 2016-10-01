93fc62688802089b3fc889a5c7b3721f9b07a319
mkfsn <p408865@gmail.com>
2016-10-01 21:01:25 +0900

    TLE version

---
+++ algorithm/perfect-squares/perfect-squares.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    int findMinPair(vector<int> counts, int start, int end) {
+        int min = counts[start++] + counts[end--];
+        while (start <= end) {
+            int tmp = counts[start++] + counts[end--];
+            if (tmp < min)
+                min = tmp;
+        }
+        return min;
+    }
+public:
+    int numSquares(int n) {
+
+        vector<int> counts(n + 1, 0);
+
+        // Init
+        for (int i = 1, j = i * i; j <= n; i++, j = i * i)
+            counts[j] = 1;
+
+        counts[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            if (counts[i] == 1)
+                continue;
+            counts[i] = findMinPair(counts, 1, i - 1);
+        }
+        
+        return counts[n];
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution s;
+    cout << s.numSquares(8829) << endl;
+    return 0;
+}
