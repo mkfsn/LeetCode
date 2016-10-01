4b1250dee39e05357b3526ebc39d5e647a412d64
mkfsn <p408865@gmail.com>
2016-10-01 22:18:40 +0900

    Reduce line

--- algorithm/perfect-squares/perfect-squares.cpp
+++ algorithm/perfect-squares/perfect-squares.cpp
@@ -7,8 +7,7 @@ class Solution {
 public:
     int numSquares(int n) {
         static vector<int> dp{0};
-        for (int i = dp.size(); i <= n; ++i) {
-            int squares = INT_MAX;
+        for (int i = dp.size(), squares = INT_MAX; i <= n; ++i) {
             for (int j = 1; j * j <= i; ++j)
                 squares = min(squares, dp[i - j * j] + 1);                            
             dp.push_back(squares);
